0bc9ae67524148a9dae8ea26595f438ebe00f1d5
Aad1trao <54617644+Aad1trao@users.noreply.github.com>
2019-09-28 19:45:45 +0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -15,7 +15,7 @@ if choice == '+':
     print(num1, "+", num2, "=", add(num1,num2))
 
 if choice == '-':
-    print(num1, "-", num2, "=", substract(num1,num2))
+    print(num1, "-", num2, "=", subtract(num1,num2))
 
 if choice == '*':
     print(num1, "*", num2, "=", multiply(num1,num2))
